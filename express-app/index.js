ee6859882d13c66f24588c2cfe7ebe5bf31ebf8c
steepl <50868613+steepl@users.noreply.github.com>
2026-06-16 13:05:48 +0200

    Added h2 to test Watchtower

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1><h2>Hello Watchtower</h2>')
 })
 
 const PORT = 8080
